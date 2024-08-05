d8541c4c0ed5b7469152a6da15ef28dca575753c
bhaaratkrishnan <bhaaratkrishnan29113@gmail.com>
2024-08-05 20:56:00 +0530

    deploy script logs and docker ignore file

--- deploy.sh
+++ deploy.sh
@@ -14,9 +14,11 @@ SQL_INSTANCE_NAME="yu-start-instance"
 DATABASE_NAME="default-db"
 
 # Set shell config
+echo "Configuring shell"
 gcloud config set project $PROJECT_ID
 
 # Enabling APIs 
+echo "Enabling APIs"
 gcloud services enable cloudbuild.googleapis.com \
 run.googleapis.com \
 artifactregistry.googleapis.com \
@@ -25,6 +27,7 @@ sql-component.googleapis.com \
 sqladmin.googleapis.com 
 
 # Creating Service Account
+echo "Creating Service Account"
 gcloud iam service-accounts create yu-start-service-acc 
 
 gcloud projects add-iam-policy-binding $PROJECT_ID \
@@ -49,12 +52,14 @@ yu-start-service-acc@$PROJECT_ID.iam.gserviceaccount.com \
 --role="roles/iam.serviceAccountUser"
 
 # Create Cloud SQL Instance
+echo "Creating Cloud SQL Instance"
 gcloud sql instances create $SQL_INSTANCE_NAME --edition="enterprise" --database-version="POSTGRES_15"  --region=$LOCATION --tier="db-f1-micro"
 
 gcloud sql users create $DATABASE_USERNAME --instance=$SQL_INSTANCE_NAME --password=$DATABASE_PASSWORD 
 gcloud sql databases create $DATABASE_NAME --instance=$SQL_INSTANCE_NAME
 
 # Migration 
+echo "Running Migration"
 curl -o cloud-sql-proxy https://storage.googleapis.com/cloud-sql-connectors/cloud-sql-proxy/v2.12.0/cloud-sql-proxy.linux.amd64
 
 chmod +x cloud-sql-proxy
@@ -67,15 +72,29 @@ npm i prisma -y
 
 npx prisma migrate dev --name init
 
+fuser -k 2229/tcp
+# Creating ENV file
+touch .env
+
+cat > .env <<EOF
+PROJECT_ID="$PROJECT_ID"
+LOCATION="$LOCATION"
+DATABASE_USERNAME="$DATABASE_USERNAME"
+DATABASE_PASSWORD="$DATABASE_PASSWORD"
+DATABASE_NAME="$DATABASE_NAME"
+DATABASE_INSTANCE_NAME="$PROJECT_ID:$LOCATION:$SQL_INSTANCE_NAME"
+EOF
+
 # Create Artifact Resgistry
+echo "Creating Artifact Registry"
 gcloud artifacts repositories create yu-start-repo --repository-format=docker --location=$LOCATION
 
 DOCKER_URL=$LOCATION-docker.pkg.dev/$PROJECT_ID/yu-start-repo/yu-start-image
 
-docker build . -t $DOCKER_URL
+docker build . -t $DOCKER_URL 
 
 docker push $DOCKER_URL
 
 # Deploy to Cloud Run 
-
+echo "Deploying to Cloud Run"
 gcloud run deploy yu-start --allow-unauthenticated --platform=managed --region=$LOCATION --image=$DOCKER_URL --port=3000 --service-account=$SERVICE_ACCOUNT_EMAIL --set-env-vars PROJECT_ID="$PROJECT_ID",LOCATION="$LOCATION",DATABASE_USERNAME="$DATABASE_USERNAME",DATABASE_PASSWORD="$DATABASE_PASSWORD",DATABASE_NAME="$DATABASE_NAME",DATABASE_INSTANCE_NAME="$PROJECT_ID:$LOCATION:$SQL_INSTANCE_NAME"
